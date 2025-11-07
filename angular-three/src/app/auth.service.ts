ee7989c4ed7a2f17fbc3638ac0c00575714cae74
João Tiago Ferraz Monteiro <1230762@isep.ipp.pt>
2025-11-07 22:25:45 +0000

    us 3.2.1 #106 finished

--- angular-three/src/app/auth.service.ts
+++ angular-three/src/app/auth.service.ts
@@ -1,31 +1,29 @@
-﻿import { Injectable } from '@angular/core';
+﻿import {Injectable, signal} from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private _token: string | null = null;
-  private _userName: string | null = null;
-
-  constructor() {}
+  private _token = signal<string | null>(null);
+  private _userName = signal<string | null>(null);
 
   setToken(token: string, userName: string | null) {
-    this._token = token;
-    this._userName = userName;
+    this._token.set(token);
+    this._userName.set(userName);
   }
 
   get token(): string | null {
-    return this._token;
+    return this._token();
   }
 
   get isLoggedIn(): boolean {
-    return !!this._token;
+    return !!this._token();
   }
 
   get userName(): string | null {
-    return this._userName;
+    return this._userName();
   }
 
   logout() {
-    this._token = null;
-    this._userName = null;
+    this._token.set(null);
+    this._userName.set(null);
   }
 }
